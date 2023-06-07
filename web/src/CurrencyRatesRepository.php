313340259ecdd30b35e1d7961479e505696cffd0
marwal <mariusz.waldoch98@gmail.com>
2023-06-07 18:54:33 +0200

    Add test for handling form

--- web/src/CurrencyRatesRepository.php
+++ web/src/CurrencyRatesRepository.php
@@ -34,7 +34,7 @@ class CurrencyRatesRepository
             id INT AUTO_INCREMENT PRIMARY KEY,
             source_currency VARCHAR(3) NOT NULL,
             target_currency VARCHAR(3) NOT NULL,
-            amount DECIMAL(10, 2) NOT NULL,
+            amount DECIMAL(10, 2) NOT NULL
         )";
 
         $this->connection->exec($createTableQuery);
--- web/tests/FormHandlerTest.php
+++ web/tests/FormHandlerTest.php
@@ -5,18 +5,26 @@ use Task\Currencies\DbConnection;
 use Task\Currencies\CurrencyRatesRepository;
 use Task\Currencies\FormHandler;
 
+use PDO;
+
 class FormHandlerTest extends TestCase
 {
 
     private $formHandler;
+    private $currenciesRepository;
+    private $dbConnection;
 
     protected function setUp(): void
     {
         $this->formHandler = new FormHandler();
+        $this->dbConnection = new DbConnection();
+        $this->currenciesRepository = new CurrencyRatesRepository($this->dbConnection);
     }
 
     protected function tearDown(): void
     {
+        $connection = $this->dbConnection->connectToDatabase();
+        $connection->exec("DELETE FROM conversions WHERE amount = 85.88");
     }
 
     public function testConvertCurrency()
@@ -34,5 +42,23 @@ class FormHandlerTest extends TestCase
 
         $this->assertStringNotContainsString($output, '');
         $this->assertIsFloat($convertedAmount);
+    }
+
+    public function testHandleForm()
+    {
+        $amount = $_POST['amount'] = 100.00;
+        $sourceCurrency = $_POST['source_currency'] = 'USD';
+        $targetCurrency = $_POST['target_currency'] = 'EUR';
+
+        $convertedAmount = $this->formHandler->convertCurrency($amount, $sourceCurrency, $targetCurrency);
+
+        $this->currenciesRepository->createConversionsTable();
+
+        $this->currenciesRepository->saveCurrencyConversion($sourceCurrency, $targetCurrency, $convertedAmount);
+
+        $statement = $this->dbConnection->connectToDatabase()->query('SELECT * FROM conversions WHERE amount = 85.88');
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        $this->assertNotNull($result);
     }
 }
